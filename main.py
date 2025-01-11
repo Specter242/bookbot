b91f92eff7cad353fe4378c0b6ba1d5bda210592
Specter242 <aaroneast@tlacil.com>
2025-01-10 19:16:23 -0500

    count characters

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ def main():
 
         #print(file_contents)
 
-    def count(file_contents):
+    def count_words(file_contents):
 
         words = file_contents.split()  
 
@@ -14,6 +14,28 @@ def main():
 
         print(a)
         #return a
+
+    def count_characters(file_contents):
+
+        lowered = file_contents.lower()
+
+        letters = {}
+
+        for letter in lowered:
     
+            if letter in letters:
+
+                letters[letter] = (letters[letter] + 1)
+
+            else:
+
+                letters[letter] = 1
+
+        print(letters)
+
+    
+    #count_words(file_contents)
+
+    count_characters(file_contents)
 
 main()
